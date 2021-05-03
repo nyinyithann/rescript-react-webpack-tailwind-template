7642284de6046e2d446b9be97f42b212ad0ac1a2
nyinyithann <nyinyithann@gmail.com>
2021-05-03 09:24:03 +0800

    added jit mode to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: ['./src/index.html', './src/*.{js,jsx}', './src/**/*.{js,jsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
