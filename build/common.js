512f313cee41d53667eae892677eca8750b88c4d
nyinyithann <nyinyithann@gmail.coml>
2021-12-31 23:47:30 +0800

    added bundle splitting

--- build/common.js
+++ build/common.js
@@ -7,10 +7,36 @@ const CopyPlugin = require('copy-webpack-plugin');
 const isProductionMode = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.js'),
+  entry: {
+    main: path.resolve(__dirname, '..', './src/index.js'),
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, '..', './dist'),
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
   },
 
   module: {
